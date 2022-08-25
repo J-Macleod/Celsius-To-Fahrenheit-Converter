bd8497d69a8a8b06f0474f5e269e32af0dda2316
Jacob Macleod <94126853+J-Macleod@users.noreply.github.com>
2022-08-25 02:26:41 -0400

    Added main..py

---
+++ main.py
@@ -0,0 +1,12 @@
+def convert_c(c):
+
+    f = ((9/5) * float(c)) + 32
+    print("Fahrenheit: "+str(f))
+
+up = True
+
+while up:
+    print("================================================")
+    c = input("Celsius: ")
+    convert_c(c)
+
